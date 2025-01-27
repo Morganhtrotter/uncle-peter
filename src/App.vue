4725fb3f44fd007c006eb0ffc3b379a443fc4d52
Morgan Trotter <mhtrotte@ucsc.edu>
2025-01-26 23:18:27 -0800

    Created dev-1 branch

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Changing text again!" />
+      <HelloWorld msg="Another change for production!" />
     </div>
   </header>
 
