b3b037b73a77502a3794ed5197576c0d71618ebd
Morgan Trotter <morganhtrotter@gmail.com>
2025-02-13 23:03:59 -0800

    Added PrimeVue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,6 +15,7 @@ defineProps({
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
+    <Button class="form-button p-button-sm" label="Button">HELLO WORLD</Button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Button from "primevue/button"
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.component('Button', Button);
+
+app.mount('#app');
