de515f370b777cbc540dc065d60c39fb5bde803a
Morgan Trotter <morganhtrotter@gmail.com>
2025-02-14 17:39:44 -0800

    Vue router working with Menubar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { ref } from "vue";
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const items = ref([
     {
         label: 'Home',
-        icon: 'pi pi-home'
+        icon: 'pi pi-home',
+        route: '/'
     },
     {
-        label: 'Features',
-        icon: 'pi pi-star'
+        label: 'About',
+        icon: 'pi pi-star',
+        route: '/about'
     },
     {
         label: 'Projects',
@@ -44,7 +49,8 @@ const items = ref([
     },
     {
         label: 'Contact',
-        icon: 'pi pi-envelope'
+        icon: 'pi pi-envelope',
+        route: '/contact'
     }
 ]);
 
@@ -60,11 +66,21 @@ defineProps({
   <div class="header-wrapper">
     <h1 class="green">{{ msg }}</h1>
     <div class="card">
-        <Menubar :model="items" />
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </div>
+        <Menubar :model="items">
+            <template #item="{ item, props, hasSubmenu }">
+                <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+                    <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                        <span :class="item.icon" />
+                        <span>{{ item.label }}</span>
+                    </a>
+                </router-link>
+                <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+                    <span :class="item.icon" />
+                    <span>{{ item.label }}</span>
+                    <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
+                </a>
+            </template>
+        </Menubar>
     </div>
   </div>
 </template>
