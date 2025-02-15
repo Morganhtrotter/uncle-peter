ce3b06c328dcf442a1f56afcd65e84ceef71a9f3
Morgan Trotter <35540710+Morganhtrotter@users.noreply.github.com>
2025-02-15 12:15:51 -0800

    Created navbar with Primevue and vue router (#1)
    * Created dev-1 branch
    
    * Added PrimeVue
    
    * Installing primeicons and making header horizontal
    
    * Simple Vue router working, no Menubar support yet
    
    * Vue router working with Menubar
    
    * Full width background color on menubar
    
    * Top banner background color banner full width
    
    * Styling top navbar

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,18 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
+  <div class="titleWrapper">
+    <h1 class="green headerTitle">Peter <span>Paige</span></h1>
+  </div>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="Another change for production!" />
+      <HelloWorld />
     </div>
   </header>
 
   <main>
     <TheWelcome />
+    <router-view />
   </main>
 </template>
 
@@ -28,12 +30,6 @@ header {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
   .logo {
     margin: 0 2rem 0 0;
   }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import './assets/main.css'
+import './assets/main.css';
+import "primeicons/primeicons.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Button from "primevue/button";
+import Menubar from 'primevue/menubar';
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.component('Button', Button);
+app.component('Menubar', Menubar);
+
+app.mount('#app');
